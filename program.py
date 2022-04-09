56f68e2e102a688da5874a86ac4aae4f3565ee80
ccrr88 <103311962+ccrr88@users.noreply.github.com>
2022-04-09 16:41:38 +0200

    Update program.py

--- program.py
+++ program.py
@@ -3,6 +3,7 @@ import tweepy
 from datetime import date
 
 def main():
+
     #Read security tokens from external .ini file
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -12,15 +13,15 @@ def main():
     #Pass tokens for authorization
     auth = tweepy.OAuthHandler(config['API']['Key'], config['API']['Secret'])
     auth.set_access_token(config['Access']['Token'], config['Access']['Secret'])
-
+    
     #Create API object
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    #Get the user object
+     #Get the user object
     user = api.get_user(screen_name=config['General']['ScreenName'])
 
     print(user.screen_name)
-    print(config['General']['ScreenName'] + ' has ' + str(user.followers_count) + ' friends')
+    print(config['General']['ScreenName'] + ' has ' + str(user.friends_count) + ' friends')
 
     #List to put the friends in
     friends_to_unfollow = [];
@@ -40,11 +41,12 @@ def main():
         print('Unfollowing %s friends..' % len(friends_to_unfollow))
 
         #To unfollow wothout prompt, comment the following 2 lines
-        answer = input("Are you sure? [Y/n]").lower()
+        answer = input("Are you sure? [y/n]").lower()
         if answer and answer[0] == "y":
             for friend in friends_to_unfollow:
                 print("Unfollowing " + friend.screen_name)
                 friend.unfollow()
+
 
 
 if __name__ == "__main__":
